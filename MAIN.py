1df3c7110c20aca1e01396ab6fc3b6a30f957462
ververln <48061251+ververln@users.noreply.github.com>
2020-03-04 20:10:22 +0100

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,4 +1,4 @@
-#This simple Python code takes a row from your Excel dataset and applies VADER Sentiment Analyzer to it by looping through each row and displaying the compound, positive, neutral and negative sentiment each in separate row next to the original data.
+#This simple Python code takes a column from your Excel dataset and applies VADER Sentiment Analyzer to it by looping through each row and displaying the compound, positive, neutral and negative sentiment each in separate row next to the original data.
 #Replace your_path\data.csv with path on your own PC.
 
 #If you are using this code, please credit.
